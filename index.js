422c404a496264e91e1cb6f9045f9f86756bf195
bobertsworld <bobertsworld@gmail.com>
2022-11-04 10:09:57 -0700

    commit tree 962b045315221df23d8abe39fba772b168f77065

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -10,8 +11,27 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+
+}
+  // console.log(books);
+  // const allPages = document.getElementsByTagName('books.length')
+  function renderBooks(bookPages) {
+    const main2 = document.querySelector('main');
+    bookPages.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.numberOfPages;
+      main2.appendChild(h2);
+    })
+
+
+
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
